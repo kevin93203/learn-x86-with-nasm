6263389d9ca901fad3b90cf4753ec78a4ec889ad
kevin93203 <kevin93203@gmail.com>
2025-12-29 14:55:59 +0000

    print a message in BIOS

--- JazzOS/README.md
+++ JazzOS/README.md
@@ -1,2 +1,14 @@
+1. 
+```bash
 make
+```
+
+2. 
+```bash
 qemu-system-i386 -fda build/main.img
+# or
+qemu-system-i386 -fda build/main.img -nographic -serial mon:stdio
+```
+
+3. if use -nographic -serial mon:stdio
+to quit the qemu press `CTRL+A` and then press `X`
--- JazzOS/src/main.asm
+++ JazzOS/src/main.asm
@@ -1,12 +1,43 @@
 ; src/main.asm
-ORG 0x7C00
-BITS 16
+ORG 0x7C00	; the code will be load to memory address: 0x7C00
+BITS 16		; means 16 bit real mode
 
 main:
-    HLT
+	MOV ax, 0		; ax is 16 bits
+	MOV ds, ax		; data segment set to 0
+	MOV es, ax		; extra segment set to 0
+	MOV ss, ax		; stack segment set to 0
+	MOV sp, 0x7C00		; stack pointer set to 0x7C00
+	MOV si, os_boot_msg	; si: source index, os_boot_msg is the address of the message, also means the offset
+	CALL print
+	HLT
 
 halt:
     JMP halt
 
-TIMES 510-($-$$) DB 0
-DW 0AA55h
+print:
+	PUSH si
+	PUSH ax
+	PUSH bx
+
+print_loop:
+	LODSB		; load string byte (1 byte) from [ds:si] to al, the physical address is at (ds * 16 + si), LODSB will auto increase the si for one byte
+			; LODSB same as "MOV al, [si], INC si"
+	OR al, al	; means is al is 0 (null terminator)?
+	JZ done_print	; jump to done_print if al is 0
+
+	MOV ah, 0x0E	; ah: 0x0E means print a char to the screen (teletype mode)
+	MOV bh, 0	; page number, means display at screen 0
+	INT 0x10	; video service interrupt (bios)
+	JMP print_loop
+
+done_print:
+	POP bx
+	POP ax
+	POP si
+	RET		; return to the next line of CALL print (HALT)
+
+os_boot_msg: DB 'Our OS has booted!', 0x0D, 0x0A, 0	; 0x0D: (Carriage Return, CR), 0x0A: (Line Feed, LF), 0 (Null Terminator)
+
+TIMES 510-($-$$) DB 0	; TIMES 510-($-$$) means repeat 510-($-$$) times, $ means the address of this line, $$ means the beginning address of this section
+DW 0AA55h	; 2 bytes, x86 is little-endian, BIOS checks whether a sector is bootable, it looks at the last two bytes to see if they are 0x55AA
