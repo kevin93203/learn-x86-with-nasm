04559c65d129676a542132a84c0417f4aae72e3f
kevin93203 <kevin93203@gmail.com>
2025-12-31 20:24:51 +0000

    Creating a FAT12 Disk

--- JazzOS/src/bootloader/boot.asm
+++ JazzOS/src/bootloader/boot.asm
@@ -1,6 +1,32 @@
-; src/main.asm
+; src/bootloader/boot.asm
 ORG 0x7C00	; the code will be load to memory address: 0x7C00
 BITS 16		; means 16 bit real mode
+
+JMP SHORT main ; JMP SHORT  (for small, nearby jumps)
+NOP			;  No Operation (to force memory alignment)
+
+; BIOS Parameter Block (BPB)
+bdb_oem: DB 'MSWIN4.1'
+bdb_bytes_per_sector: DW 512
+bdb_sectors_per_cluster: DB 1
+bdb_reserved_sectior: DW 1
+bdb_fat_count: DB 2
+bdb_dir_entries_count: DW 0E0h
+bdb_total_sectors: DW 2880
+bdb_media_descriptor_type: DB 0F0h
+bdb_sectors_per_fat: DW 9
+bdb_sectors_per_track: DW 18
+bdb_heads: DW 2
+bdb_hidden_sections: DD 0
+bdb_large_sector_count: DD 0
+
+; (Extended Boot Record, EBR)
+ebr_drive_number: DB 0
+				  DB 0
+ebr_signature:	  DB 29h
+ebr_volume_id:	  DB 12h,34h,56h,78h
+ebr_volume_label:  DB 'JASS OS    '	; 11 chars
+ebr_system_id:	   DB 'FAT12   '	; 8 chars
 
 main:
 	MOV ax, 0		; ax is 16 bits
